cca4d29f03c6215ad8c92432c04f769f657c1170
Ofer Tauber <ofertauber@gmail.com>
2022-05-01 21:50:51 +0300

    classes split

--- scripts/card.js
+++ scripts/card.js
@@ -1,5 +1,17 @@
-export function Card(uniqueId, game) {
-  this.uniqueId = uniqueId;
-  this.pairId = undefined;
+export function Card(id, game) {
+  this.id = id;
   this.game = game;
+  this.element; //todo create the element and add chrt apropriate CSS classes
 }
+
+Card.prototype.pauseGame = function () {
+  //TODO
+};
+
+Card.prototype.flipCard = function () {
+  //TODO
+};
+
+Card.prototype.clickCard = function () {
+  //TODO
+};
