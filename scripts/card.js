4c00cf38006537049779d3f59ac79b702c9daec8
naamagi <n.gilat@gmail.com>
2022-05-02 11:20:25 +0300

    update

--- scripts/card.js
+++ scripts/card.js
@@ -1,5 +1,28 @@
 export function Card(uniqueId, game) {
   this.uniqueId = uniqueId;
-  this.pairId = undefined;
   this.game = game;
+  this.cardElement = document.querySelector(`.card`+uniqueId);
+  this.faceUp=false;
+  // this.cardElement.addEventListener("click", this);
+ 
 }
+
+// game
+// pairNum (int)
+// faceUp – (bool)
+// cardElement
+// isMached (bool)
+
+// }
+
+Card.prototype.click =function(){
+this.game.handleClick(this);
+}
+
+
+
+
+const flipCard=()=>{
+
+}
+
